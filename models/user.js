3a61c3fa4b26302f76558dbed4fc70ba2831c8bc
Brett Hartman <brett.hartman@mac.com>
2016-05-07 00:37:01 -0700

    bug fix

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@
 var mongoose = require('mongoose');
 var jwt = require('jsonwebtoken');
 
-const JWT_SECRET = process.env.JWT_SECRET || 'This is my secret. It is long and unguessable. It can be as long as you want.';
+const JWT_SECRET = process.env.JWT_SECRET;
 
 var userSchema = new mongoose.Schema({
     username: {
